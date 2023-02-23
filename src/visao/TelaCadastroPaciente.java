74d8224c45eb9dddc1c49131c18fb51f4d294ec3
Bernardo <benckebernardo@gmail.com>
2023-02-23 11:57:52 -0300

    CadastroPaciente MarcarConsulta

--- src/visao/TelaCadastroPaciente.java
+++ src/visao/TelaCadastroPaciente.java
@@ -7,10 +7,25 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import java.awt.Toolkit;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.SystemColor;
+import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
 
 public class TelaCadastroPaciente extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtInserir;
+	private JTextField txtInserir_6;
+	private JTextField txtInserir_1;
+	private JTextField txtInserir_2;
+	private JTextField txtInserir_3;
+	private JTextField txtInserir_5;
+	private JTextField txtInserir_4;
+	private JTextField txtAdicionarInformaes;
 
 	/**
 	 * Launch the application.
@@ -41,6 +56,128 @@ public class TelaCadastroPaciente extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBounds(531, 11, 306, 508);
+		contentPane.add(panel);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setLayout(null);
+		panel_1.setBackground(Color.WHITE);
+		panel_1.setBounds(69, 57, 171, 191);
+		panel.add(panel_1);
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setIcon(new ImageIcon(TelaCadastroPaciente.class.getResource("/img/iconpeople.png")));
+		lblNewLabel.setBounds(52, 44, 72, 96);
+		panel_1.add(lblNewLabel);
+		
+		txtInserir = new JTextField();
+		txtInserir.setText("Inserir");
+		txtInserir.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		txtInserir.setColumns(10);
+		txtInserir.setBounds(10, 274, 217, 20);
+		panel.add(txtInserir);
+		
+		txtInserir_6 = new JTextField();
+		txtInserir_6.setText("Inserir");
+		txtInserir_6.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		txtInserir_6.setColumns(10);
+		txtInserir_6.setBounds(11, 466, 214, 20);
+		panel.add(txtInserir_6);
+		
+		txtInserir_1 = new JTextField();
+		txtInserir_1.setText("Inserir");
+		txtInserir_1.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		txtInserir_1.setColumns(10);
+		txtInserir_1.setBounds(10, 313, 217, 20);
+		panel.add(txtInserir_1);
+		
+		txtInserir_2 = new JTextField();
+		txtInserir_2.setText("Inserir");
+		txtInserir_2.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		txtInserir_2.setColumns(10);
+		txtInserir_2.setBounds(10, 349, 86, 20);
+		panel.add(txtInserir_2);
+		
+		txtInserir_3 = new JTextField();
+		txtInserir_3.setText("Inserir");
+		txtInserir_3.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		txtInserir_3.setColumns(10);
+		txtInserir_3.setBounds(106, 349, 86, 20);
+		panel.add(txtInserir_3);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nome");
+		lblNewLabel_1.setBounds(10, 262, 46, 14);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Nome social");
+		lblNewLabel_2.setBounds(10, 300, 86, 14);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Sexo");
+		lblNewLabel_3.setBounds(10, 337, 46, 14);
+		panel.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_6 = new JLabel("Nascimento");
+		lblNewLabel_6.setBounds(106, 336, 115, 14);
+		panel.add(lblNewLabel_6);
+		
+		JLabel lblNewLabel_7 = new JLabel("E-mail");
+		lblNewLabel_7.setBounds(12, 451, 46, 14);
+		panel.add(lblNewLabel_7);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setIcon(new ImageIcon(TelaCadastroPaciente.class.getResource("/img/iconCheck.png")));
+		btnNewButton.setBackground(SystemColor.menu);
+		btnNewButton.setBounds(237, 440, 59, 57);
+		panel.add(btnNewButton);
+		
+		txtInserir_5 = new JTextField();
+		txtInserir_5.setText("Inserir");
+		txtInserir_5.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		txtInserir_5.setColumns(10);
+		txtInserir_5.setBounds(9, 425, 214, 20);
+		panel.add(txtInserir_5);
+		
+		JLabel lblNewLabel_5_1 = new JLabel("CEP");
+		lblNewLabel_5_1.setBounds(11, 410, 90, 14);
+		panel.add(lblNewLabel_5_1);
+		
+		txtInserir_4 = new JTextField();
+		txtInserir_4.setText("Inserir");
+		txtInserir_4.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		txtInserir_4.setColumns(10);
+		txtInserir_4.setBounds(11, 386, 115, 20);
+		panel.add(txtInserir_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Telefone");
+		lblNewLabel_5.setBounds(10, 372, 90, 14);
+		panel.add(lblNewLabel_5);
+		
+		txtAdicionarInformaes = new JTextField();
+		txtAdicionarInformaes.setText("Adicionar Informações");
+		txtAdicionarInformaes.setHorizontalAlignment(SwingConstants.CENTER);
+		txtAdicionarInformaes.setForeground(Color.WHITE);
+		txtAdicionarInformaes.setFont(new Font("Yu Gothic UI Light", Font.BOLD, 14));
+		txtAdicionarInformaes.setEditable(false);
+		txtAdicionarInformaes.setColumns(10);
+		txtAdicionarInformaes.setBackground(new Color(64, 128, 128));
+		txtAdicionarInformaes.setBounds(0, 0, 306, 36);
+		panel.add(txtAdicionarInformaes);
+		
+		JLabel lblNewLabel_8 = new JLabel("");
+		lblNewLabel_8.setIcon(new ImageIcon(TelaCadastroPaciente.class.getResource("/img/iconmedicina.png")));
+		lblNewLabel_8.setBounds(87, -77, 369, 532);
+		contentPane.add(lblNewLabel_8);
+		
+		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.setForeground(Color.WHITE);
+		btnVoltar.setBackground(new Color(64, 128, 128));
+		btnVoltar.setBounds(10, 482, 110, 37);
+		contentPane.add(btnVoltar);
 	}
 
 }
---
+++ src/visao/TelaMarcarConsultas.java
@@ -0,0 +1,178 @@
+package visao;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.SystemColor;
+import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
+
+public class TelaMarcarConsultas extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField txtMarcarConsulta;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaMarcarConsultas frame = new TelaMarcarConsultas();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaMarcarConsultas() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 862, 569);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(219, 219, 219));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBounds(10, 122, 306, 290);
+		contentPane.add(panel);
+		
+		textField = new JTextField();
+		textField.setText("Inserir");
+		textField.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		textField.setColumns(10);
+		textField.setBounds(11, 59, 217, 20);
+		panel.add(textField);
+		
+		textField_1 = new JTextField();
+		textField_1.setText("Inserir");
+		textField_1.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		textField_1.setColumns(10);
+		textField_1.setBounds(12, 251, 214, 20);
+		panel.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setText("Inserir");
+		textField_2.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		textField_2.setColumns(10);
+		textField_2.setBounds(11, 98, 217, 20);
+		panel.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setText("Inserir");
+		textField_3.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		textField_3.setColumns(10);
+		textField_3.setBounds(11, 134, 115, 20);
+		panel.add(textField_3);
+		
+		JLabel lblNewLabel_1 = new JLabel("Paciente");
+		lblNewLabel_1.setBounds(11, 47, 46, 14);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Profissional");
+		lblNewLabel_2.setBounds(11, 85, 86, 14);
+		panel.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Data");
+		lblNewLabel_3.setBounds(11, 122, 46, 14);
+		panel.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_7 = new JLabel("E-mail");
+		lblNewLabel_7.setBounds(13, 236, 46, 14);
+		panel.add(lblNewLabel_7);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setIcon(new ImageIcon(TelaMarcarConsultas.class.getResource("/img/iconCheck.png")));
+		btnNewButton.setBackground(SystemColor.menu);
+		btnNewButton.setBounds(238, 225, 59, 57);
+		panel.add(btnNewButton);
+		
+		textField_5 = new JTextField();
+		textField_5.setText("Inserir");
+		textField_5.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		textField_5.setColumns(10);
+		textField_5.setBounds(10, 210, 214, 20);
+		panel.add(textField_5);
+		
+		JLabel lblNewLabel_5_1 = new JLabel("Forma de Pagamento");
+		lblNewLabel_5_1.setBounds(12, 195, 191, 14);
+		panel.add(lblNewLabel_5_1);
+		
+		textField_6 = new JTextField();
+		textField_6.setText("Inserir");
+		textField_6.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
+		textField_6.setColumns(10);
+		textField_6.setBounds(12, 171, 115, 20);
+		panel.add(textField_6);
+		
+		JLabel lblNewLabel_5 = new JLabel("Objetivo");
+		lblNewLabel_5.setBounds(11, 157, 90, 14);
+		panel.add(lblNewLabel_5);
+		
+		txtMarcarConsulta = new JTextField();
+		txtMarcarConsulta.setText("Marcar Consulta");
+		txtMarcarConsulta.setHorizontalAlignment(SwingConstants.CENTER);
+		txtMarcarConsulta.setForeground(Color.WHITE);
+		txtMarcarConsulta.setFont(new Font("Yu Gothic UI Light", Font.BOLD, 14));
+		txtMarcarConsulta.setEditable(false);
+		txtMarcarConsulta.setColumns(10);
+		txtMarcarConsulta.setBackground(new Color(64, 128, 128));
+		txtMarcarConsulta.setBounds(0, 0, 306, 36);
+		panel.add(txtMarcarConsulta);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(new Color(0, 81, 81));
+		panel_1.setBounds(0, 0, 846, 53);
+		contentPane.add(panel_1);
+		
+		JPanel panel_1_1 = new JPanel();
+		panel_1_1.setBackground(new Color(0, 81, 81));
+		panel_1_1.setBounds(0, 477, 846, 53);
+		contentPane.add(panel_1_1);
+		
+		JPanel panel_1_2 = new JPanel();
+		panel_1_2.setBackground(new Color(64, 128, 128));
+		panel_1_2.setBounds(0, 52, 846, 28);
+		contentPane.add(panel_1_2);
+		
+		JPanel panel_1_2_1 = new JPanel();
+		panel_1_2_1.setBackground(new Color(64, 128, 128));
+		panel_1_2_1.setBounds(0, 451, 846, 28);
+		contentPane.add(panel_1_2_1);
+		
+		JLabel lblNewLabel_8 = new JLabel("");
+		lblNewLabel_8.setIcon(new ImageIcon(TelaMarcarConsultas.class.getResource("/img/iconmedicina.png")));
+		lblNewLabel_8.setBounds(413, -81, 369, 532);
+		contentPane.add(lblNewLabel_8);
+		
+		JButton btnVoltar = new JButton("Voltar");
+		btnVoltar.setFont(new Font("Yu Gothic UI Light", Font.BOLD, 11));
+		btnVoltar.setForeground(Color.WHITE);
+		btnVoltar.setBackground(new Color(64, 128, 128));
+		btnVoltar.setBounds(747, 417, 89, 23);
+		contentPane.add(btnVoltar);
+	}
+
+}
