54171995c421341b4d9aa235543e1d2e1eb5ac0f
Bernardo <benckebernardo@gmail.com>
2023-03-09 12:01:55 -0300

    Redimensão

--- src/visao/TelaListaPaciente.java
+++ src/visao/TelaListaPaciente.java
@@ -22,6 +22,9 @@ import javax.swing.JTable;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import net.miginfocom.swing.MigLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
 
 public class TelaListaPaciente extends JFrame {
 
@@ -48,42 +51,42 @@ public class TelaListaPaciente extends JFrame {
 		setTitle("Hospital Esmeralda");
 		setIconImage(Toolkit.getDefaultToolkit().getImage(TelaListaPaciente.class.getResource("/img/logoHospital.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 853, 572);
+		setBounds(100, 100, 997, 845);
 		contentPane = new JPanel();
 		contentPane.setBackground(new Color(219, 219, 219));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(TelaListaPaciente.class.getResource("/img/iconPesquisa.png")));
-		lblNewLabel.setBounds(343, 24, 47, 39);
-		contentPane.add(lblNewLabel);
+		contentPane.setLayout(new MigLayout("", "[][540.00px,grow][495.00px,grow]", "[28.00px,fill][19px][49.00][342.00px,grow][31px]"));
 		
 		txtPesquisarPaciente = new JTextField();
 		txtPesquisarPaciente.setForeground(new Color(128, 128, 128));
 		txtPesquisarPaciente.setText("Pesquisar paciente");
 		txtPesquisarPaciente.setToolTipText("");
 		txtPesquisarPaciente.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 11));
-		txtPesquisarPaciente.setBounds(51, 22, 331, 39);
-		contentPane.add(txtPesquisarPaciente);
+		contentPane.add(txtPesquisarPaciente, "cell 1 0,growx,aligny bottom");
 		txtPesquisarPaciente.setColumns(10);
+		
+		panel_1 = new JPanel();
+		contentPane.add(panel_1, "cell 1 2,grow");
+		panel_1.setBackground(new Color(64, 128, 128));
 		
 		panel = new JPanel();
 		panel.setBackground(new Color(255, 255, 255));
-		panel.setBounds(51, 82, 489, 388);
-		contentPane.add(panel);
-		panel.setLayout(null);
-		
-		panel_1 = new JPanel();
-		panel_1.setBackground(new Color(64, 128, 128));
-		panel_1.setBounds(0, 0, 489, 33);
-		panel.add(panel_1);
+		contentPane.add(panel, "cell 1 3,grow");
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{471, 0};
+		gbl_panel.rowHeights = new int[]{638, 0};
+		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
 		
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(0, 34, 489, 354);
-		panel.add(scrollPane);
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 0;
+		gbc_scrollPane.gridy = 0;
+		panel.add(scrollPane, gbc_scrollPane);
 		
 		table = new JTable();
 		scrollPane.setViewportView(table);
@@ -102,13 +105,11 @@ public class TelaListaPaciente extends JFrame {
 		btnSelecionar.setForeground(new Color(255, 255, 255));
 		btnSelecionar.setFont(new Font("Yu Gothic UI Semibold", Font.PLAIN, 11));
 		btnSelecionar.setBackground(new Color(64, 128, 128));
-		btnSelecionar.setBounds(572, 491, 213, 31);
-		contentPane.add(btnSelecionar);
+		contentPane.add(btnSelecionar, "cell 2 4,alignx right,aligny top");
 		
 		lblNewLabel_1 = new JLabel("");
 		lblNewLabel_1.setIcon(new ImageIcon(TelaListaPaciente.class.getResource("/img/iconmedicina.png")));
-		lblNewLabel_1.setBounds(503, -66, 282, 525);
-		contentPane.add(lblNewLabel_1);
+		contentPane.add(lblNewLabel_1, "cell 2 0 1 4,grow");
 		
 		btnVoltar = new JButton("Voltar");
 		btnVoltar.setForeground(new Color(255, 255, 255));
@@ -123,8 +124,7 @@ public class TelaListaPaciente extends JFrame {
 						telaPadrao.setExtendedState(JFrame.MAXIMIZED_BOTH);
 			}
 		});
-		btnVoltar.setBounds(20, 499, 89, 23);
-		contentPane.add(btnVoltar);
+		contentPane.add(btnVoltar, "cell 1 4,alignx left,aligny top");
 		
 	}
 }
