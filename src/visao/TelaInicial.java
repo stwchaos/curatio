b10f3e1e62ba17ff39d5522fa50c4eb5ce8d47a3
Pfleger <pflegerdev@gmail.com>
2023-03-16 16:58:21 -0300

    update front tela login

--- src/visao/TelaInicial.java
+++ src/visao/TelaInicial.java
@@ -77,15 +77,24 @@ public class TelaInicial extends JFrame {
 		}
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1008, 658);
+		setBounds(100, 100, 1031, 800);
+		BufferedImage bg = null;
 		
-		JPanel panelPrincipal = new JPanel();
+		try {
+			bg = ImageIO.read(new File("src/img/fundoTelaLogin.png"));
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		JPanel panelPrincipal = new PanelComBackgroundImage(bg);;
+		
 
 		panelPrincipal.setBackground(new Color(0, 81, 81));
 		panelPrincipal.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(panelPrincipal);
-		panelPrincipal.setLayout(new MigLayout("", "[grow][389.00px][25.00,grow]", "[grow][362.00px][grow]"));
+		panelPrincipal.setLayout(new MigLayout("", "[grow][499.00px][25.00,grow]", "[grow][553.00px,grow][grow]"));
 
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(64, 128, 128));
@@ -96,10 +105,10 @@ public class TelaInicial extends JFrame {
 		panel.add(lblNewLabel2);
 
 		
-		BufferedImage bg = null;
-		;
+
+		
 		try {
-			bg = ImageIO.read(new File("src/img/fundoLogin-removebg-preview (2).png"));
+			bg = ImageIO.read(new File("src/img/fundoLogin.png"));
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -107,53 +116,60 @@ public class TelaInicial extends JFrame {
 		}
 		JPanel c = new PanelComBackgroundImage(bg);;
 		c.setForeground(null);
-		c.setBackground(new Color (0,81,81));
+		c.setBackground(new Color(0, 81, 81));
 		panel.add(c);
-		c.setLayout(new MigLayout("", "[62.00,grow][38.00][184.00][113.00,grow]", "[][73.00,grow][17.00][][64.00][][][82.00,grow][56.00]"));
+		c.setLayout(new MigLayout("", "[62.00,grow][68.00][249.00][68.00][62.00,grow]", "[][132.00,grow][17.00][grow][27.00][][][28.00][52.00,grow][32.00][56.00]"));
 				
 				JLabel lblNewLabel = new JLabel("");
-				lblNewLabel.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/users_icon (3).png")));
-				c.add(lblNewLabel, "cell 2 0");
+				lblNewLabel.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/iconLogin.png")));
+				c.add(lblNewLabel, "cell 2 0,alignx center,aligny center");
+				
+				JLabel lblNewLabel_3 = new JLabel("Login Curatio");
+				lblNewLabel_3.setBackground(new Color(0, 47, 47));
+				lblNewLabel_3.setFont(new Font("Yu Gothic Medium", Font.PLAIN, 17));
+				lblNewLabel_3.setForeground(new Color(255, 255, 255));
+				c.add(lblNewLabel_3, "cell 2 1,alignx center,aligny center");
 		
 				JLabel lblNewLabel_1 = new JLabel("CPF");
+				lblNewLabel_1.setBackground(new Color(0, 47, 47));
 				lblNewLabel_1.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Carinha Apenas.png")));
-				c.add(lblNewLabel_1, "cell 1 4,growx,aligny center");
-				lblNewLabel_1.setForeground(new Color(0, 0, 0));
+				c.add(lblNewLabel_1, "cell 1 3 2 1,growx,aligny bottom");
+				lblNewLabel_1.setForeground(new Color(255, 255, 255));
 
 		txtCpf = new RoundJTextField();
 		txtCpf.setHorizontalAlignment(SwingConstants.CENTER);
-		txtCpf.setBackground(new Color(64, 128, 128));
-		txtCpf.setForeground(new Color(255, 255, 255));
+		txtCpf.setBackground(new Color(255, 255, 255));
+		txtCpf.setForeground(new Color(0, 47, 47));
 		txtCpf.setCaretColor(Color.WHITE);
 		// txtCpf.setBorder(BorderFactory.createEmptyBorder());
-		c.add(txtCpf, "cell 2 4,growx");
+		c.add(txtCpf, "cell 1 4 3 1,grow");
 
 		txtCpf.setToolTipText("");
 		txtCpf.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 12));
 		txtCpf.setColumns(10);
-		
-				JLabel lblNewLabel_2 = new JLabel("Senha");
-				lblNewLabel_2.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Cadeado.png")));
-				c.add(lblNewLabel_2, "cell 1 6,alignx left,aligny center");
-				lblNewLabel_2.setForeground(new Color(0, 0, 0));
+				
+						JLabel lblNewLabel_2 = new JLabel("Senha");
+						lblNewLabel_2.setIcon(new ImageIcon(TelaInicial.class.getResource("/img/Cadeado.png")));
+						c.add(lblNewLabel_2, "cell 1 6 2 1,alignx left,aligny center");
+						lblNewLabel_2.setForeground(new Color(255, 255, 255));
 		
 				txtSenha = new RoundPasswordField();
 				txtSenha.setHorizontalAlignment(SwingConstants.CENTER);
-				txtSenha.setForeground(new Color(255, 255, 255));
-				txtSenha.setBackground(new Color(64, 128, 128));
+				txtSenha.setForeground(new Color(0, 47, 47));
+				txtSenha.setBackground(new Color(255, 255, 255));
 				txtSenha.setCaretColor(Color.WHITE);
 				txtSenha.setSelectedTextColor(new Color(0, 0, 0));
 				
-						c.add(txtSenha, "cell 2 6,growx");
+						c.add(txtSenha, "cell 1 7 3 1,grow");
 
 		JButton btnEntrar = new JButton("Entrar");
-		btnEntrar.setBackground(new Color (0,81,81));
+		btnEntrar.setBackground(new Color(0, 81, 81));
 		btnEntrar.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		btnEntrar.setFocusPainted(false);
 		btnEntrar.setForeground(new Color(255, 255, 255));
 		btnEntrar.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 17));
 
-		c.add(btnEntrar, "cell 2 7,growx,aligny bottom");
+		c.add(btnEntrar, "cell 2 9,growx,aligny center");
 		btnEntrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
@@ -166,8 +182,8 @@ public class TelaInicial extends JFrame {
 					senha = txtSenha.getText();
 				}
 				ProfissionalDAO profissionalDAO = new ProfissionalDAO();
-				Profissional p = profissionalDAO.efetuarLogin(cpf, senha);
-				if (p != null ) {
+                Profissional p = profissionalDAO.efetuarLogin(cpf, senha);
+                if ( p != null ) {
 
 //					if (p.getCpfProfissionais().equals(cpf) && p.getSenha().equals(senha)) {
 					dispose();
@@ -177,7 +193,7 @@ public class TelaInicial extends JFrame {
 					telaPadrao.setExtendedState(JFrame.MAXIMIZED_BOTH);
 //					}
 				} else {
-					new DialogMensagemErro("Autentificao invalida").setVisible(true);
+					new DialogMensagemErro("Senha incorreta!").setVisible(true);
 					return;
 				}
 			}
