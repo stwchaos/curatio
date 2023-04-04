34f0d6449f80c1b26d425043896beb63005417a7
Gabriel do Rosario Batista <rosariogabriel484@gmail.com>
2023-04-04 09:08:40 -0300

    Comeco PagamentoDAO

--- src/controle/PagamentoDAO.java
+++ src/controle/PagamentoDAO.java
@@ -1,5 +1,38 @@
 package controle;
 
-public class PagamentoDAO {
+import modelo.Pagamento;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+public class PagamentoDAO {
+	private Conexao con;
+	
+	public boolean inserir(Pagamento p) {
+		
+		con = Conexao.getInstancia();
+		
+		Connection c = con.conectar();
+		
+		try {
+			String query = "INSERT INTO pagamento (forma_pagamento,data_pagamento) VALUES (?,?);";
+			PreparedStatement stm = c.prepareStatement(query);
+			
+			stm.setString(1,p.getFormaPagamento());
+			stm.setString(2,p.getData_Pagamento());
+			
+			stm.executeUpdate();
+			return true;
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		return false;
+	}
+	
+	
 }
