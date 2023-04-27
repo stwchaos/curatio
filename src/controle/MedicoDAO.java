dd184f33bcf32aa19a78f0d8deae162df1a2321a
Gabriel do Rosario Batista <rosariogabriel484@gmail.com>
2023-04-27 08:37:53 -0300

    Test MedicoDAO

--- src/controle/MedicoDAO.java
+++ src/controle/MedicoDAO.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
 
 import modelo.Especialidade;
 import modelo.Medico;
---
+++ src/test/MedicoDAOTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MedicoDAOTest {
+	
+	
+	@Test
+	void test() {
+		
+		fail("Not yet implemented");
+	
+	}
+	
+
+}
