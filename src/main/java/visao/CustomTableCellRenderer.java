9ceb4164703cdfd43c901449a6cdb4715e69582c
Pfleger Mendes <pflegerdev@gmail.com>
2023-06-01 10:19:16 -0300

    fonte branca

--- src/main/java/visao/CustomTableCellRenderer.java
+++ src/main/java/visao/CustomTableCellRenderer.java
@@ -8,25 +8,27 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 
 class CustomTableCellRenderer extends DefaultTableCellRenderer {
-	private ArrayList<Integer> faltas;
+    private ArrayList<Integer> faltas;
 
-	public CustomTableCellRenderer(ArrayList<Integer> faltas) {
-		this.faltas = faltas;
-	}
+    public CustomTableCellRenderer(ArrayList<Integer> faltas) {
+        this.faltas = faltas;
+    }
 
-	@Override
-	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
-			int row, int column) {
-		Component cellComponent = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-
-		// Verifica se a linha atual está na lista de faltas
-		if (faltas.contains(row)) {
-			cellComponent.setBackground(Color.RED);
-		} else {
-			// Define a cor de fundo padrão das linhas não faltantes
-			cellComponent.setBackground(table.getBackground());
-		}
-
-		return cellComponent;
-	}
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        Component cellComponent = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        
+        // Verifica se a linha atual está na lista de faltas
+        if (faltas.contains(row)) {
+            cellComponent.setBackground(Color.RED);
+            cellComponent.setForeground(Color.WHITE); // Define a cor da fonte como branca
+        } else {
+            // Define a cor de fundo padrão e a cor da fonte padrão das linhas não faltantes
+            cellComponent.setBackground(table.getBackground());
+            cellComponent.setForeground(table.getForeground());
+        }
+        
+        return cellComponent;
+    }
 }
+
